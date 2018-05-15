cb288c546539d43cf21ff417f1bf088989830f4b
kodekat <kodekat@macbookpro.local>
2018-05-14 21:20:53 -0700

    some UI color changes

--- MatchingCards/NumberGame.swift
+++ MatchingCards/NumberGame.swift
@@ -10,8 +10,8 @@ import Foundation
 import UIKit
 
 class NumberGame: UIViewController {
-    
-    private var guessNumber = 0
+    let colors = Colors()
+    private var actualNumber = 0
     private var countGuesses = 0
     private var guessAgain = false
     
@@ -19,18 +19,27 @@ class NumberGame: UIViewController {
     @IBOutlet weak var guessLabel: UILabel!
     
     @IBAction func guessTheNum(_ sender: Any) {
+        
+        if guessAgain {
+            countGuesses = 0
+            guessText.text = ""
+            actualNumber = Int(arc4random_uniform(100))
+            guessAgain = false
+        }
+        
         if (Int(guessText.text!) != nil){
             //input is present
             countGuesses += 1
             let number = Int(guessText.text!)
-            if number == guessNumber {
-                guessLabel.text = "Congrats You Guessed Correctly! It took you \(countGuesses) times to get it right. Do you want to Guess Again?"
-            }else if number! < guessNumber{
-                guessLabel.text = "You're a little far off. Try going up."
+            if number == actualNumber {
+                guessLabel.text = "Congrats You Guessed Correctly! It took you \(countGuesses) times. Do you want to Play Again?"
+                guessAgain = true
+            }else if number! < actualNumber{
+                guessLabel.text = "You're a little far off.\n Try going up."
                 
-            }else if number! > guessNumber{
+            }else if number! > actualNumber{
                 
-                guessLabel.text = "You're a little far off. Try going down."
+                guessLabel.text = "You're a little far off.\n Try going down."
                 
             }
             guessText.text = "";
@@ -42,9 +51,33 @@ class NumberGame: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guessNumber = Int(arc4random_uniform(100))
+        loadGradientColors()
+        actualNumber = Int(arc4random_uniform(100))
+        //reset random value
     }
     
     
+    func loadGradientColors() {
+        view.backgroundColor = UIColor.clear
+        var backgroundLayer = colors.gl
+        backgroundLayer?.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer!, at: 0)
+    }
+
+
     
+    
+}
+
+class Colors {
+    var gl:CAGradientLayer!
+    
+    init() {
+        let colorTop = UIColor(red: 42.0 / 255.0, green: 134.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 226.0 / 255.0, green: 242.0 / 255.0, blue: 249.0 / 255.0, alpha: 1.0).cgColor
+        
+        self.gl = CAGradientLayer()
+        self.gl.colors = [colorTop, colorBottom]
+        self.gl.locations = [0.0, 1.0]
+    }
 }
