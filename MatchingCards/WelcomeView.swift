1137ba8833a0c8416e158ae84e7202ba21bdf57c
kodekat <kodekat@macbookpro.local>
2018-05-15 18:20:06 -0700

    added an image

---
+++ MatchingCards/WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeView.swift
+//  MatchingCards
+//
+//  Created by Teodora on 5/15/18.
+//  Copyright © 2018 Teodora. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeView: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.img.image = UIImage(named: "cloudIcon")
+    }
+    
+    
+}
